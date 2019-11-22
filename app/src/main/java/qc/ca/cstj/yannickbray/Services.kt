8f385fe1a91698e8dc8e2c68c2ba37892d96cd7a
Jérémie Ouimet <1568282@cstj.qc.ca>
2019-11-22 07:59:47 -0500

    Adresse IP VM Android Studio

--- app/src/main/java/qc/ca/cstj/yannickbray/Services.kt
+++ app/src/main/java/qc/ca/cstj/yannickbray/Services.kt
@@ -2,16 +2,16 @@ package qc.ca.cstj.yannickbray
 
 class Services {
     companion object {
-        // val API_URL_GET_CATEGORIE = "http://10.0.2.2:4500/categories/"
-        val API_URL_GET_CATEGORIE = "http://172.17.100.2:4500/categories/"
+        val API_URL_GET_CATEGORIE = "http://10.0.2.2:4500/categories/"
+        // val API_URL_GET_CATEGORIE = "http://172.17.100.2:4500/categories/"
 
-        // val API_URL_GET_SUCCURSALE = "http://10.0.2.2:4500/succursales/"
-        val API_URL_GET_SUCCURSALE = "http://172.17.100.2:4500/succursales/"
+        val API_URL_GET_SUCCURSALE = "http://10.0.2.2:4500/succursales/"
+        // val API_URL_GET_SUCCURSALE = "http://172.17.100.2:4500/succursales/"
 
-        // val API_URL_GET_LIVRE_BY_CATEGORIE = "http://10.0.2.2:4500/livres?categorie="
-        val API_URL_GET_LIVRE_BY_CATEGORIE = "http://172.17.100.2:4500/livres?categorie="
+        val API_URL_GET_LIVRE_BY_CATEGORIE = "http://10.0.2.2:4500/livres?categorie="
+        // val API_URL_GET_LIVRE_BY_CATEGORIE = "http://172.17.100.2:4500/livres?categorie="
 
-        // val API_URL = "http://10.0.2.2:4500/"
-        val API_URL = "http://172.17.100.2:4500/"
+        val API_URL = "http://10.0.2.2:4500/"
+        // val API_URL = "http://172.17.100.2:4500/"
     }
 }
