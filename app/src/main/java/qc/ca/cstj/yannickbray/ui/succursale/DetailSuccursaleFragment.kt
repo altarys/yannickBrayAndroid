dbc9ab5a35b36014c5120385a95d2cbbaf07afef
Merlin <bohdan.byrne@me.com>
2019-11-22 23:08:01 -0500

    commentairesA

--- app/src/main/java/qc/ca/cstj/yannickbray/ui/succursale/DetailSuccursaleFragment.kt
+++ app/src/main/java/qc/ca/cstj/yannickbray/ui/succursale/DetailSuccursaleFragment.kt
@@ -16,7 +16,7 @@ import qc.ca.cstj.yannickbray.R
  * A simple [Fragment] subclass.
  */
 class DetailSuccursaleFragment : Fragment() {
-
+    // Succursale obtenu en paramètre
     private val args: DetailSuccursaleFragmentArgs by navArgs()
  
 
@@ -30,13 +30,16 @@ class DetailSuccursaleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Assigne les différentes propriétés de l'objet succursale à des champs de texte
         view.txvAppelatif.text = args.succursale.appelatif
         view.txvAdresse.text = args.succursale.adresse
         view.txvVille.text = args.succursale.ville +"(" + args.succursale.province + ")"
         view.txvCodePostal.text = args.succursale.codePostal
+        // Téléphone et télécopieur son afficher en partie pour l'ajout de caractères spéciaux ( paranthèse, trait d'union etc )
         view.txvTelephone.text = "Téléphone : (" + args.succursale.telephone.subSequence(0,3) +") " + args.succursale.telephone.subSequence(4,7) + " -" +args.succursale.telephone.subSequence(7,12)
         view.txvTelecopieur.text = "Télécopieur : (" + args.succursale.telecopieur.subSequence(0,3)+") " + args.succursale.telecopieur.subSequence(4,7) + " -" +args.succursale.telecopieur.subSequence(7,12)
         view.txvInformation.text = "Informations : \n" + args.succursale.information
+        // Horaire de la succursale hardcoder
         view.txvHoraire.text = "Lundi et Mardi : 9 h 30 à 18 h \nMercredi, Jeudi et Vendredi : 9 h 30 à 21 h \nSamedi : 9 h à 17 h \nDimanche : 10 h à 17 h"
     }
 
