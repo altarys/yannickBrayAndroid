9321388409747d857611f4f976384193a92d0465
jayouimet <jayouimet@hotmail.com>
2019-11-22 23:20:29 -0500

    commentaires jayo

--- app/src/main/java/qc/ca/cstj/yannickbray/Services.kt
+++ app/src/main/java/qc/ca/cstj/yannickbray/Services.kt
@@ -2,6 +2,8 @@ package qc.ca.cstj.yannickbray
 
 class Services {
     companion object {
+        // Jérémie Ouimet : J'ai un processeur AMD, donc j'utilise NOX et son localhost est 172.17.100.2
+
         val API_URL_GET_CATEGORIE = "http://10.0.2.2:4500/categories/"
         // val API_URL_GET_CATEGORIE = "http://172.17.100.2:4500/categories/"
 
